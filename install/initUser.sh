029db7afe71c2d5b69233b6f805e9479c57b21c9
tasgit <tasgit@1000kit.org>
2017-04-05 12:22:28 +0200

    add creation of initial directory structure

--- install/initUser.sh
+++ install/initUser.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
+
+
+## USER:PASSWORD:UID/GID:ENCRYPTED:DIRS (,separated)
 #echo "init User: $SFTP_USERS"
 # Add users
 IFS=',' read -a users <<< "$SFTP_USERS"
 for userData in "${users[@]}"; do
     IFS=':' read -a data <<< "$userData"
+    
     user="${data[0]}"
     pass="${data[1]}"
 
-   if [ "${data[3]}" == "e" ]; then
-      encrypted=yes
+    if [ ! -z "${data[2]}" ]; then
       uid="${data[2]}"
-    elif [ "${data[2]}" == "e" ]; then
+    fi   
+    if [ "${data[3]}" == "e" ]; then
       encrypted=yes
-    elif [ ${data[2]} ]; then
-      uid="${data[2]}"
     fi
-
+    if [ ! -z "${data[4]}" ]; then 
+      createDirs=${data[4]}
+    fi
     if ! [ $uid ]; then
-      uid=$(shuf -i 1500-2000 -n 1)
+        uid=$(shuf -i 1500-2000 -n 1)
     fi
+   
     #echo "Add User $uid $user"
-    useradd -u $uid $user
+    useradd -u $uid $user -d /home/${user}
 
     #echo "change passwd: $user:$pass"
     if [ "$encrypted" == "yes" ]; then
@@ -30,8 +35,16 @@ for userData in "${users[@]}"; do
       echo "$user:$pass" | chpasswd
     fi
 
-    #chown root:root /home/$user
-    #chmod 755 /home/$user
+    if [ ! -z "${createDirs}" ]; then
+       IFS=";"
+       for dd in ${createDirs}; do
+           echo "--> mkdir -p /home/${user}/${dd}"
+           mkdir -p /home/${user}/${dd}
+       done
+    fi
+    
+    chown -R ${user}:${user} /home/${user}
+    chmod 755 /home/${user}
 done
 
 exec "$@"
